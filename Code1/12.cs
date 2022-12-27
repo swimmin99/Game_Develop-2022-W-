e4c76625d0cd80fec0ae8d961de3b7c229279951
HANGHAE <109887066+swimmin99@users.noreply.github.com>
2022-12-27 21:27:35 +0900

    Add files via upload

---
+++ Code1/12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code1
+{
+    internal class Position
+    {
+        public static void Main()
+        {
+            int a = 1;
+            int b = 1;
+            Console.WriteLine("전치라면 a는 " + ++a + "가 됩니다.");
+            Console.WriteLine("후치라면 b는 " + b++ + "가 됩니다.");
+        }
+    }
+}
+//전치라면 a는 2가 됩니다.
+//후치라면 b는 1가 됩니다.
---
+++ Code1/13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code1
+{
+    internal class Compare
+    {
+        public static void Main()
+        {
+            int a = 10, b = 20;
+            Console.WriteLine("a = " + a + " b = " + b);
+            Console.WriteLine("a<b ... " + (a < b));
+            Console.WriteLine("a>b ... " + (a > b));
+            Console.WriteLine("a==b ... " + (a == b));
+            Console.WriteLine("a=b ... " + (a = b));
+
+        }
+    }
+}
+
+//a = 10 b = 20
+//a < b... True
+//a>b ... False
+//a==b ... False
+//a=b ... 20
